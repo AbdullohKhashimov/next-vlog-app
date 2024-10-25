28781d383a65ad02b9779539f908e1779ae8b39b
Abdulloh Salimov <abdullohsalimov5@gmail.com>
2024-10-25 14:09:18 +0900

    feat: went through catch all segments samples by creating required component pages

---
+++ src/app/account/membership/membership-info/page.js
@@ -0,0 +1,7 @@
+export default function MembershipInfo() {
+  return (
+    <div>
+      <h1>Membership info page</h1>
+    </div>
+  );
+}
---
+++ src/app/products/[details]/page.js
@@ -0,0 +1,9 @@
+export default function ProductDetails() {
+  return (
+    <div>
+      <h1>Product details page</h1>
+    </div>
+  );
+}
+
+// bracket [] will allow us to make up dynamic routing.
